e1f85295f45a470827a11279d84983b4f6dda0b9
tony <tony@computer.com>
2023-02-22 21:54:34 +0100

    added sqlalchemy relationships and datatypes to the user model

--- models/user.py
+++ models/user.py
@@ -3,15 +3,21 @@
 Contains the User Class
 """
 
+from hashlib import md5
 from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """Representation of the User object"""
-    first_name = ""
-    last_name = ""
-    email = ""
-    password = ""
+    __tablename__ = "users"
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    restaurant_reviews = relationship("RestaurantReview", backref="users", cascade="all, delete, delete-orphan")
+    food_reviews = relationship("FoodReview", backref="users", cascade="all, delete, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         """Initialisation of the User Class"""
