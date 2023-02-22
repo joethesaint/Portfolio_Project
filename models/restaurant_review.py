507aa3ec3f5c366a603820a5ececea4050119b00
tony <tony@computer.com>
2023-02-22 21:55:24 +0100

    added sqlalchemy relationships and datatypes to the restaurantreview model

--- models/restaurant_review.py
+++ models/restaurant_review.py
@@ -4,18 +4,19 @@ Contains RestaurantReview Class
 """
 
 from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, Float, ForeignKey
 
 
 class RestaurantReview(BaseModel, Base):
     """Represents the RestaurantReview class"""
     __tablename__ = "restaurantreviews"
-    restaurant_id = ""
-    user_id = ""
-    text = ""
-    ambience = ""
-    service = ""
-    cleanliness = ""
-    uniqueness = ""
+    restaurant_id = Column(String(60), ForeignKey('restaurants.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable="False")
+    ambience = Column(Float, nullable=False)
+    service = Column(Float, nullable=False)
+    cleanliness = Column(Float, nullable=False)
+    uniqueness = Column(Float, nullable=False)
 
 
     def __init__(self, *args, **kwargs):
