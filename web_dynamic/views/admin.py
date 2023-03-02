f5c2ae831069d274b362a355d72e1fb42eeedda3
tony <tony@computer.com>
2023-03-02 11:32:06 +0100

    added little authentication to admin routes

--- web_dynamic/views/admin.py
+++ web_dynamic/views/admin.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 
-from flask import flash, redirect, request, render_template
-import json
+from flask import flash, session, request, render_template
 from models import storage
 from models.city import City
 from models.food import Food
 from models.restaurant import Restaurant
+from models.user import User
 from web_dynamic.views import app_views
 
 @app_views.route('/add_restaurant', methods=['GET', 'POST'])
 def add_restaurant():
+    if session.get('user_id', None) is None:
+        flash('You must be logged in to do this', category='info')
+        return render_template('admin/add_restaurant.html')
+
+    if storage.get(User, session.get('user_id', None)).email[:5] != "admin":
+        flash('You are not authorised to do this', category='danger')
+        return render_template('admin/add_restaurant.html')
+
     cities = storage.all(City)
     if request.method == "POST":
         if len(request.form['name']) < 3:
@@ -35,6 +43,14 @@ def add_restaurant():
 
 @app_views.route('/add_food', methods=['GET', 'POST'])
 def add_food():
+    if session.get('user_id', None) is None:
+        flash('You must be logged in to do this', category='info')
+        return render_template('admin/add_food.html')
+
+    if storage.get(User, session.get('user_id', None)).email[:5] != "admin":
+        flash('You are not authorised to do this', category='danger')
+        return render_template('admin/add_food.html')
+
     restaurants = storage.all(Restaurant)
     restaurants = sorted(restaurants, key=lambda k: k.name)
     if request.method == "POST":
